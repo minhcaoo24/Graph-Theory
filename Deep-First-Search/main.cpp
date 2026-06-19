c5cb07826dd36d6454a8b65e75839449793f8fc2
nminh123 <caonhatminh245@gmail.com>
2026-06-19 13:51:41 +0700

    Deep First Search

---
+++ Deep-First-Search/main.cpp
@@ -0,0 +1,29 @@
+#include "Dfs.hpp"
+#include "input.hpp"
+#include <cstdlib>
+
+int main(int, char **) {
+  Input i;
+  Dfs d;
+  // Input
+  /*
+    9 9
+    1 2
+    1 3
+    1 5
+    2 4
+    3 6
+    3 7
+    3 9
+    5 8
+    8 9
+  */
+
+  std::vector<int> adj[1001];
+  std::array<bool, 1001> visited;
+  std::array<int, 1001> parent;
+
+  i.inp(adj, 1, 9);
+  d.DeepFirstSearch(adj, visited, parent, 1);
+  return EXIT_SUCCESS;
+}
